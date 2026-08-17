2575f4812d41d1e938535e587fa6abca785bad6a
ngobiro <pngobiro@gmail.com>
2026-08-17 16:34:10 +0300

    Add edge runtime config to dashboard edit pages for Cloudflare Workers compatibility

--- src/app/dashboard/author/blogs/[id]/edit/page.tsx
+++ src/app/dashboard/author/blogs/[id]/edit/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import { useState, useRef, useEffect } from 'react';
 import Link from 'next/link';
 import { useParams, useRouter } from 'next/navigation';
